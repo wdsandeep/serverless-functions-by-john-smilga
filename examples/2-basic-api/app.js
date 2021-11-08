76372a5912d11c5099428de2dc3fd1a06bcdf1bd
wdsandeep <wd.sandeep@gmail.com>
2021-11-08 14:51:37 +0530

    Testing Deploy after two functions

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,30 @@
+const result = document.querySelector('.result')
+
+
+const fetchData = async () => {
+    try {
+        const { data } = await axios.get('/api/2-basic-api')
+        const products = data.map((product) => {
+
+            const { image:{ url }, name, price } = product;
+            return `<article class="product">
+            <img
+              src="${url}"
+              alt="${name}"
+            />
+            <div class="info">
+              <h5>${name}</h5>
+              <h5 class="price">$${price}</h5>
+            </div>
+          </article>`
+        }).join('');
+
+        result.innerHTML = products
+
+    }
+    catch (error) {
+        result.innerHTML = `<h2>There was an error </h2>`
+    }
+}
+
+fetchData();
---
+++ functions/1-hello.js
@@ -0,0 +1,7 @@
+// domain/.netlify/functions/1-hello
+exports.handler = async ( event, context, cb) => {
+    return {
+        statusCode:200,
+        body: 'Our First Netlify Function'
+    }
+}
